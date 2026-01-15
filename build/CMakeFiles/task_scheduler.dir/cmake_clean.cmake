e320de0c3ee1f4dd8ace39652900c79366436bb3
Joshua Ruopp <josh.ruopp@gmail.com>
2026-01-15 18:58:51 +0100

    initial commit

---
+++ build/CMakeFiles/task_scheduler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task_scheduler.dir/src/task.cpp.o"
+  "CMakeFiles/task_scheduler.dir/src/task.cpp.o.d"
+  "libtask_scheduler.a"
+  "libtask_scheduler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_scheduler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/task_scheduler.dir/src/task.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/task_scheduler.dir/src/task.cpp.o: \
+ /home/joshy/Projects/task_scheduler/src/task.cpp \
+ /usr/include/stdc-predef.h
